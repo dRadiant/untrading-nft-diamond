dd1ec0fa2b0c344cf9e2b9dda94aee8c8e22a864
Why Oh Why <103012174+dRadiant@users.noreply.github.com>
2023-01-10 18:51:42 -0800

    Deploy new contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -41,6 +41,11 @@ const config: HardhatUserConfig = {
       accounts: process.env.DEPLOYER_PRIVATE_KEY == undefined ? [] : [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
       saveDeployments: true,
     },
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_TOKEN}`, // RPC URL
+      accounts: process.env.DEPLOYER_PRIVATE_KEY == undefined ? [] : [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+      saveDeployments: true,
+    },
   },
   
   verify: {
